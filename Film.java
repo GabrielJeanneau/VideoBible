f29501f35e6391620d6f96950a4ece1afa65a485
GabrielJeanneau <79854512+GabrielJeanneau@users.noreply.github.com>
2022-01-03 17:51:02 +0100

    Liste filtré par support

---
+++ Film.java
@@ -0,0 +1,52 @@
+package com.epsi;
+
+public class Film {
+    private String nom;
+    private String acteurP;
+    private String realisateur;
+    private String anneeSortie;
+    private String support;
+    public boolean dispo = true;
+    public String locataire;
+
+    public Film(String nom, String acteurP, String realisateur, String anneeSortie, String support){
+        this.nom = nom;
+        this.acteurP = acteurP;
+        this.realisateur = realisateur;
+        this.anneeSortie = anneeSortie;
+        this.support = support;
+    }
+
+    public void show(){
+        System.out.println("Film : "+nom+", Acteur Principal : "+acteurP+", Realisateur : "+realisateur+", Année de sortie : "+anneeSortie+", support : "+support+".");
+    }
+
+    public String giveName(){
+        return this.nom;
+    }
+
+    public String giveSupport(){
+        return this.support;
+    }
+
+    public void filmLoue(){
+        if(dispo == true){
+            dispo = false;
+            System.out.println("Vous avez bien loué ce film :"+nom);
+        }
+        else if(dispo == false){
+            System.out.println("Le film "+nom+" est déjà loué.");
+        }
+    }
+
+    public void filmRendu(){
+        if(dispo == false){
+            dispo = true;
+            System.out.println("Vous avez bien rendue le film :"+nom);
+        }
+        else if(dispo == false){
+            System.out.println("Le film "+nom+" est déjà disponible.");
+        }
+    }
+
+}
